b8d8276ffbf07ea2855102933e3c4e8bd57c99a2
Joachim Toft <stonehard11@gmail.com>
2016-02-11 06:25:31 +0100

    Moves the other page out of this project, I will create a new one

--- add_movie.php
+++ add_movie.php
@@ -100,7 +100,7 @@
     function validate_director($field)
     {
         if (strlen($field) > 64) return "Stop it.";
-        elseif (!preg_match("/^[a-zA-Z 0-9]+$/", $field)) return "Invalid title. Currently only accepts a-z, A-Z and 0-9.";
+        elseif (!preg_match("/^[a-zA-Z 0-9]+$/", $field)) return "Invalid director. Currently only accepts a-z, A-Z and 0-9.";
         else return "";
     }
 
